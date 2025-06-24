5255859594f31cd8fb5ae838a37643e42b330289
Luiz Miguel Toller Marconatto <141162499+MiguelToller@users.noreply.github.com>
2025-06-24 15:51:38 -0300

    Create folder RNA and Add Project 'ProblemaTimes'

---
+++ RNA/ProblemaTimes/src/application/Program.java
@@ -0,0 +1,65 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Exemplo de RNA Perceptron para classificação de equipes");
+		
+		List<Ponto> amostras = new ArrayList<Ponto>();
+		List<Integer> saidas = new ArrayList<>();
+		
+		Scanner sc = new Scanner(System.in);
+		String caminhoArquivo = "problemaTimes.csv";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+		
+            String linha;
+            
+            while ((linha = br.readLine()) != null) {
+                String[] partes = linha.split(";");
+                if (partes.length >= 3) {
+                    double x = Double.parseDouble(partes[0]);
+                    double y = Double.parseDouble(partes[1]);
+                    int saida = Integer.parseInt(partes[2]);
+
+                    amostras.add(new Ponto(x, y));
+                    saidas.add(saida);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+
+		double taxa_aprendizado = 0.1;
+        int geracoes = 1000;
+        int limiar = 1;
+        
+        Perceptron p = new Perceptron(amostras, saidas, taxa_aprendizado, geracoes, limiar);
+		p.treinar();
+		String op;
+		
+		do 
+		{
+			System.out.println("\n\nInforme valor para x (-1 a 1): ");
+			double x = Double.parseDouble(sc.nextLine());
+			System.out.println("Informe valor para y (-1 a 1): ");
+			double y = Double.parseDouble(sc.nextLine()); 
+
+			p.testar(new Ponto(x,y));
+			System.out.println("1 - Sair: ");
+			op = sc.nextLine();
+			
+		} while (op != "1");
+		
+		sc.close();
+    }
+
+}
